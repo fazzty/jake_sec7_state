a0c6547f2249ceeede4f5653541f571aab3c951c
fazzty <42378654+fazzty@users.noreply.github.com>
2023-04-21 18:08:22 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { UserProvider } from "./providers/UserProvider";
+
+import { Router } from "./router/Router";
+import "./styles.css";
+import { RecoilRoot } from "recoil";
+
+export default function App() {
+  return (
+    <RecoilRoot>
+      <UserProvider>
+        <Router />;
+      </UserProvider>
+    </RecoilRoot>
+  );
+}
---
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -0,0 +1,42 @@
+import React, { memo } from "react";
+import styled from "styled-components";
+import { useRecoilValue } from "recoil";
+import { userState } from "../../../store/userState";
+
+export const UserIconWithName = memo((props) => {
+  console.log("UserIconWithNaem");
+
+  const { image, name } = props;
+  // const { userInfo } = useContext(UserContext);
+  const userInfo = useRecoilValue(userState);
+  const isAdmin = userInfo ? userInfo.isAdmin : false;
+
+  return (
+    <SContainer>
+      <SImg height={160} widht={160} alt={name} src={image} />
+      <SName>{name}</SName>
+      {isAdmin && <SEdit>編集</SEdit>}
+    </SContainer>
+  );
+});
+
+const SContainer = styled.div`
+  text-align: center;
+`;
+
+const SImg = styled.img`
+  border-radius: 50%;
+`;
+
+const SName = styled.p`
+  font-size: 18px;
+  font-weight: bold;
+  margin: 0;
+  color: #40514e;
+`;
+
+const SEdit = styled.span`
+  text-decoration: underline;
+  color: #aaa;
+  cursor: pointer;
+`;
